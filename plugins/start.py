87f983f9c7bcbbc78885515c07041d6c0baaef2d
PC <126058657+pcbotz@users.noreply.github.com>
2024-03-02 21:56:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,3 @@
-#(©)CodeXBotz
 
 
 
@@ -85,9 +84,14 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🌐 𝐌𝐨𝐯𝐢𝐞 𝐆𝐫𝐨𝐮𝐩 🌐", url = "https://t.me/pcmoviegroup")],
-                       InlineKeyboardButton("📡 𝐂𝐡𝐚𝐧𝐧𝐞𝐥  📡", url = "https://t.me/pcott"),
-                       InlineKeyboardButton("👑 𝐀𝐝𝐦𝐢𝐧 👑" , url = "https://t.me/PCADMINOFFICIALBOT"))],
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("🌐 𝐌𝐨𝐯𝐢𝐞 𝐆𝐫𝐨𝐮𝐩 🌐", url = "https://t.me/pcmoviegroup"),
+                    InlineKeyboardButton("📡 𝐂𝐡𝐚𝐧𝐧𝐞𝐥  📡", url = "https://t.me/pcott")
+                ]
+            ]
+        )
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
