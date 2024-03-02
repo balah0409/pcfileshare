24a663a6cc26a7a5187418bdebfcae7e987b2d5b
PC <126058657+pcbotz@users.noreply.github.com>
2024-03-02 12:45:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🌐 𝐌𝐨𝐯𝐢𝐞 𝐆𝐫𝐨𝐮𝐩 🌐", url = "https://t.me/pcmoviegroup"),
+                    InlineKeyboardButton("📡 𝐂𝐡𝐚𝐧𝐧𝐞𝐥  📡", url = "https://t.me/pcott")
                 ]
             ]
         )
